f91a7042eaccdce937b591d00b0c71a6ace6d387
achadha2 <andy.chadha2@gmail.com>
2019-04-29 16:05:04 -0400

    set up app to work with CSS grid

---
+++ src/Containers/CameraContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class CameraContainer extends React.Component {
+  render() {
+    return <div>This is an exciting Container for a camera</div>;
+  }
+}
+
+export default CameraContainer;
